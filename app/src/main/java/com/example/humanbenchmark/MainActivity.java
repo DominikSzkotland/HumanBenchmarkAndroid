d4b682353967d24f5127e2b3a5e026ce61b90a5a
DominikSzkotland <dszlencka@outlook.com>
2024-05-24 19:48:24 +0200

    click listeners added

--- app/src/main/java/com/example/humanbenchmark/MainActivity.java
+++ app/src/main/java/com/example/humanbenchmark/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.humanbenchmark;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button reactionTestButton;
+    Button simonTestButton;
+    Button numberSequenceButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        reactionTestButton = findViewById(R.id.reactionTimeButton);
+        simonTestButton = findViewById(R.id.simonMemoryTest);
+        numberSequenceButton = findViewById(R.id.numberSequenceTestButton);
+
+        reactionTestButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+
+        simonTestButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+
+        numberSequenceButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
     }
 }
