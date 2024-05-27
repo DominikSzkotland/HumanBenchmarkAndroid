f5cae5d457f5ce8b229e13bf1d464edb9527ab23
DominikSzkotland <dszlencka@outlook.com>
2024-05-27 19:11:59 +0200

    Simon game files created

---
+++ app/src/main/java/com/example/humanbenchmark/SimonMemoryTest.java
@@ -0,0 +1,14 @@
+package com.example.humanbenchmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimonMemoryTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simon_memory_test);
+    }
+}
