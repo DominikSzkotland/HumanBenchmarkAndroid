2884be98413d129ef743752030d9c8e29e58bfb3
DominikSzkotland <dszlencka@outlook.com>
2024-05-28 20:03:20 +0200

    activity files created

---
+++ app/src/main/java/com/example/humanbenchmark/NumberSequenceTest.java
@@ -0,0 +1,14 @@
+package com.example.humanbenchmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NumberSequenceTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_sequence_test);
+    }
+}
